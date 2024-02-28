7d5f2382b866ab47362f540133dc7f7096aa3aa0
N <34268416+ziloen@users.noreply.github.com>
2024-02-29 00:25:29 +0800

    Add react type override

---
+++ src/types/modules.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="react" />
+
+declare namespace React {
+  // fix react forwardRef, https://stackoverflow.com/questions/58469229/react-with-typescript-generics-while-using-react-forwardref
+  function forwardRef<T, P = {}>(
+    render: (props: P, ref: ForwardedRef<T>) => ReactElement | null
+  ): (props: P & RefAttributes<T>) => ReactElement | null
+
+  // allow style to use css custom properties
+  interface CSSProperties {
+    [CSSCutomProperties: `--${string}`]: string | number | undefined
+  }
+
+  // allow destructor return value
+  function useEffect(
+    effect: () => void | (() => void),
+    deps?: DependencyList
+  ): void
+  function useInsertionEffect(
+    effect: () => void | (() => void),
+    deps?: DependencyList
+  ): void
+  function useLayoutEffect(
+    effect: () => void | (() => void),
+    deps?: DependencyList
+  ): void
+}
