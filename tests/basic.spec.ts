d5923cf098022c6d142e8f007966d336c8c365bf
N <34268416+ziloen@users.noreply.github.com>
2024-04-24 18:15:42 +0800

    Update test

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -2,6 +2,11 @@ import { expect } from '@playwright/test'
 import { testExt } from './base'
 
 testExt('Open sidepanel page', async ({ context, page, extensionId }) => {
-  await page.goto(`chrome-extension://${extensionId}/pages/options/index.html`)
+  const optionsUrl = `chrome-extension://${extensionId}/pages/options/index.html`
+  const sidebarUrl = `chrome-extension://${extensionId}/pages/sidebar/index.html`
+
+  await page.goto(optionsUrl)
   await expect(page.locator('#root h1')).toHaveText('Options Page')
+
+  // await context.waitForEvent('page', page => page.url().startsWith(sidebarUrl))
 })
