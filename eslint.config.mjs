80ffcbe91c241da236c4e8b646a0ec0638febbe0
N <34268416+ziloen@users.noreply.github.com>
2024-06-15 22:25:57 +0800

    Update deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,7 @@ import { react } from '@ziloen/eslint-config'
 
 /** @type { import("@ziloen/eslint-config").FlatESLintConfig[] } */
 export default [
-  ...react({ tsconfigPath: ['./src/tsconfig.json', './tsconfig.json'] }),
+  ...react({
+    project: ['./src/tsconfig.json', './tsconfig.json'],
+  }),
 ]
