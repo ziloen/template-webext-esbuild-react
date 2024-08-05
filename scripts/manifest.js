2a634440d23dc2150cd245d0248edcbd9a3f109b
N <34268416+ziloen@users.noreply.github.com>
2024-08-05 17:26:38 +0800

    unwrap wrapper fn

--- scripts/manifest.js
+++ scripts/manifest.js
@@ -2,9 +2,5 @@ import fs from 'fs-extra'
 import { getManifest } from '../src/manifest'
 import { r } from './utils'
 
-function writeManifest() {
-  console.log('write manifest')
-  fs.writeJSONSync(r('dist/dev/manifest.json'), getManifest(), { spaces: 2 })
-}
-
-writeManifest()
+console.log('write manifest')
+fs.writeJSONSync(r('dist/dev/manifest.json'), getManifest(), { spaces: 2 })
