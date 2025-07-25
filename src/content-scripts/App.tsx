5ae3dfb4a4f7809cd4228794a6b1ff8a12c9d444
N <34268416+ziloen@users.noreply.github.com>
2025-07-25 14:25:54 +0800

    remove test code

--- src/content-scripts/App.tsx
+++ src/content-scripts/App.tsx
@@ -1,18 +1,3 @@
-import { ReactShadow } from '~/components/ReactShadow'
-
 export function App() {
-  if (IS_PROD) return null
-
-  return (
-    <>
-      {/* FIXME: remove dev test code */}
-      <ReactShadow>
-        <div className="bg-red z-max fixed top-0 left-0 size-20">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. A ut eveniet
-          vero magni nobis. Fuga corporis rem sint. Tempora tenetur esse et,
-          dignissimos minima exercitationem animi porro qui at rem?
-        </div>
-      </ReactShadow>
-    </>
-  )
+  return null
 }
