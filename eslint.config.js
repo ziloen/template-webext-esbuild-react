763047927c136f1e9f7ccb8f7763af1356895f40
N <34268416+ziloen@users.noreply.github.com>
2025-12-01 19:06:55 +0800

    chore: type

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,6 @@
 import { react } from '@ziloen/eslint-config'
 
-/** @type { import("@ziloen/eslint-config").ConfigArray } */
-const config = react({
+/** @type {import("@ziloen/eslint-config").ConfigArray} */
+export default react({
   project: ['./tsconfig.json', './tsconfig.app.json', './tsconfig.node.json'],
 })
-
-export default config
